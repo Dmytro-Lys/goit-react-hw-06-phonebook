7a4ca7d0714065e22c19210672a25ecc6e470223
Dmytro-Lys <d.lysenko122@gmail.com>
2023-08-18 17:03:06 +0300

    add selector getVisibleContacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,10 @@
 import css from './ContactList.module.css'
 import { ContactItem } from '../ContactItem/ContactItem';
-
 import { useSelector } from "react-redux";
-import { getContacts, getFilter } from 'redux/selectors';
-
-const filterContacts = (contacts, filter) => contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
-
+import { getVisibleContacts } from 'redux/selectors';
 
 export const ContactList = () => {
-    const contacts = useSelector(getContacts);
-    const filter = useSelector(getFilter);
-    const visibleContacts = filterContacts(contacts, filter)
+    const visibleContacts = useSelector(getVisibleContacts)
     return (
     <>
         {visibleContacts && 
